df9a96cead5aed9572b4ee7931911ad67834b4f2
leandra <leandra.reid@gmail.com>
2018-07-18 18:56:15 -0400

    updated css & finalized

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/app/public"));
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
